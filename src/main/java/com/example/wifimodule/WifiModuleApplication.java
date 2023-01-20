7fcfa14a190059a5066f3a8f79a5f6e1319db167
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-01-20 15:22:47 +0330

    init project

---
+++ src/main/java/com/example/wifimodule/WifiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.wifimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WifiModuleApplication.class, args);
+	}
+
+}

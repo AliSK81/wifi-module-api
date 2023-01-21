bab422482630c2d4f6003af6dbd430a158a251e6
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-01-21 09:47:07 +0330

    remove wifi/state from root

--- src/main/java/com/example/wifimodule/controller/StateController.java
+++ src/main/java/com/example/wifimodule/controller/StateController.java
@@ -9,23 +9,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/wifi")
 @RequiredArgsConstructor
 public class StateController {
 
     private final StateService stateService;
 
-    @PostMapping("/state")
+    @PostMapping("/")
     public ResponseEntity<State> saveTime(@RequestBody StateDto dto) {
         State state = State.builder().onn(dto.getOn()).off(dto.getOff()).build();
         State savedState = stateService.saveState(state);
         return ResponseEntity.ok(savedState);
     }
 
-    @GetMapping("/state")
+    @GetMapping("/")
     public ResponseEntity<String> getTime() {
         State state = stateService.getState();
         return ResponseEntity.ok(state.toString());
